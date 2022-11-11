d30dd348a150ac49aa852c67edef246a5737e80f
aggieju <a.juszczuk86@gmail.com>
2022-11-11 10:11:01 +0100

    sign up form send a checkbox: isArtist

--- routers/space.js
+++ routers/space.js
@@ -112,5 +112,21 @@ router.post("/stories", auth, async (req, res, next) => {
     return res.status(201).send({ message: "Story created", story });
 });*/
 
+// PATCH - update space details
+router.patch("/:id", auth, async (req, res) => {
+    const space = await Space.findByPk(req.params.id);
+    if (!space.userId === req.user.id) {
+        return res
+            .status(403)
+            .send({ message: "You are not authorized to update this space" });
+    }
+
+    const { title, description, backgroundColor, color } = req.body;
+
+    await space.update({ title, description, backgroundColor, color });
+
+    return res.status(200).send({ space });
+});
+
 module.exports = router
 
